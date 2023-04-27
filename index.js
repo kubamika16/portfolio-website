b54387c64ee0effcaf465f10ea6b31599b017c2b
kubamika16 <kuba.mika16@gmail.com>
2023-04-27 22:55:01 +0100

    commit

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const codeBlocker = () => {
-  // return new Promise((resolve, reject) => {
-  //   let i = 0
-  //   while (i < 1000000000) i++
-  //   resolve('Billion loops done')
-  // })
-
-  return Promise.resolve().then((v) => {
-    let i = 0
-    while (i < 1000000000) i++
-    return 'Billion loops done'
-  })
+const getFruit = async (name) => {
+  const fruits = {
+    pinaple: '🍍',
+    peach: '🍑',
+    strawbery: '🍓',
+  }
+  return fruits[name]
 }
 
-console.log('Synchronous 1')
-
-console.log(codeBlocker().then())
-
-console.log('Synchronous 2')
-
-const hello = async function () {
-  console.log('hello world')
-}
-
-hello()
+getFruit('peach').then(console.log)
