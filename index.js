fbe341c28523196d1cd31b269b89fab6cf4de3ee
kubamika16 <kuba.mika16@gmail.com>
2023-05-01 11:13:36 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.getElementById('closeButton').addEventListener('click', function () {
   document.querySelector('.overlay').style.display = 'none'
+  localStorage.setItem('overlayHiddenAt', Date.now())
 })
+
+// Check if the overlay should be displayed
+const overlayHiddenAt = localStorage.getItem('overlayHiddenAt')
+const timeElapsed = Date.now() - (overlayHiddenAt || 0)
+const fiveMinutesInMilliseconds = 10000
+
+if (timeElapsed < fiveMinutesInMilliseconds) {
+  document.querySelector('.overlay').style.display = 'none'
+} else {
+  document.querySelector('.overlay').style.display = 'flex'
+}
