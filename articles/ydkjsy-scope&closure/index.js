dc4cf98244458b97804484a1fcd2de802f724b0d
kubamika16 <kuba.mika16@gmail.com>
2023-06-07 10:59:03 +0100

    commit

--- articles/ydkjsy-scope&closure/index.js
+++ articles/ydkjsy-scope&closure/index.js
@@ -6,7 +6,8 @@ import {
   DATASET,
 } from '../../functions.js'
 
-const folderName = 'ydkjy-get-started'
+const folderName = 'ydkjsy-scope&closure'
+
 // Taking data from Sanity.io where article is equal to one that has folder named "ydkjy-get-started"
 const QUERY = encodeURIComponent(
   `*[_type == "post" && folder == "${folderName}"]`,
@@ -17,9 +18,9 @@ const PROJECT_URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/
 // Fetching actual data from Sanity.io
 fetchData(PROJECT_URL)
   .then((data) => {
+    console.log(data)
     const body = data.result[0].body
     console.log(body)
-
     let formattedText = formatSanityBody(body)
     document.querySelector('.article-content').innerHTML = formattedText
   })
--- functions.js
+++ functions.js
@@ -55,15 +55,6 @@ const sanityImageUrl = function (PROJECT_ID, DATASET, imageId) {
 // data with identifications to connect with Sanity.io
 let PROJECT_ID = 'x18ixioq'
 let DATASET = 'production'
-let folderName = 'ydkjy-get-started'
-
-// Taking data from Sanity.io where article is equal to one that has folder named "ydkjy-get-started"
-let QUERY = encodeURIComponent(
-  `*[_type == "post" && folder == "${folderName}"]`,
-)
-
-// That wariable helps to fetch certain dana from Sanity.io
-let PROJECT_URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`
 
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
@@ -120,8 +111,6 @@ export {
   blockStyleToHtmlTag,
   PROJECT_ID,
   DATASET,
-  folderName,
-  PROJECT_URL,
   // dateFormatter,
   formatSanityBody,
   ISODateDescructureFunction,
