62c6ac8d685f5a137d6c50ca408ad31141b9c2d7
kubamika16 <kuba.mika16@gmail.com>
2023-06-28 16:36:32 +0100

    commit

--- toggle.js
+++ toggle.js
@@ -1,14 +1,54 @@
 const toggleButton = document.querySelector('.light-toggle')
 
 toggleButton.addEventListener('click', function () {
+  // Setting up the date to current. From now on, for next couple minutes one of the themes will be on
+  let now = new Date()
+
   if (document.body.classList.contains('dark-theme')) {
+    // Because we want to change to ligh theme, we have to save that theme in local storage
+    localStorage.setItem('theme', 'light')
+    localStorage.setItem('themeTime', now.getTime())
     switchToLightTheme()
   } else if (document.body.classList.contains('light-theme')) {
+    localStorage.setItem('theme', 'dark')
+    localStorage.setItem('themeTime', now.getTime())
     switchToDarkTheme()
   } else {
+    // This is when there is no class (dark or light theme)
+    localStorage.setItem('theme', 'light')
+    localStorage.setItem('themeTime', now.getTime())
     switchToLightTheme()
   }
 })
+
+// The function to check the saved theme and its timestamp
+function applySavedTheme() {
+  let savedTheme = localStorage.getItem('theme')
+  let savedThemeTime = localStorage.getItem('themeTime')
+
+  if (savedTheme && savedThemeTime) {
+    let now = new Date()
+    let elapsedMinutes = Math.round(
+      (now.getTime() - savedThemeTime) / 1000 / 60,
+    )
+
+    // If not more than 10 minutes have passed since the theme was set
+    if (elapsedMinutes <= 10) {
+      if (savedTheme === 'light') {
+        switchToLightTheme()
+      } else {
+        switchToDarkTheme()
+      }
+    } else {
+      // If more than 10 minutes have passed, remove the saved theme
+      localStorage.removeItem('theme')
+      localStorage.removeItem('themeTime')
+    }
+  }
+}
+
+// Call the function when the page loads
+applySavedTheme()
 
 function switchToLightTheme() {
   document.body.classList.remove('dark-theme')
